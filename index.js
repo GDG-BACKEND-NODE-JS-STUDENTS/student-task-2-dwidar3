16e5a8a8721b4dc614c4d88213ed1b7128506d54
ahmed <broahmed04@gmail.com>
2025-03-10 05:35:13 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,50 @@
+var x=5;
+var y =7;
+
+console.log(x-y);
+console.log(x+y);
+console.log(x/y);
+console.log(x*y);
+console.log(x%y);
+console.log(x**y);
+
+
+
+
+
+const x=22;
+const y=12;
+
+
+if(x>y){
+    console.log(1)
+}else if (x<y){
+    console.log(2)
+}else if (x<=y){
+    console.log(3)
+}else if (x>=y){
+    console.log(4)
+}else if (x==y){
+    console.log(5)
+}else if (x===y){
+    console.log(6)
+}else if (x!==y){
+    console.log(7)
+}else;
+
+
+
+
+
+
+ let x=5;   let y=9;
+    console.log(x>y &&x<y)
+    console.log(x=y || x<y)
+    console.log(!(x>y));
+
+
+
+
+
+     let num =123;
+     console.log(String(num));
